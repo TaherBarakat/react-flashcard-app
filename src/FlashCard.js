b96d4d40f798bc7365d32b3b0db759b71f20a0c1
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-14 14:40:51 +0300

    apply style for flip action and build the flashcard component to look good

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -1,5 +1,24 @@
-import React from "react";
-
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useState } from "react";
 export default function FlashCard({ flashCard }) {
-     return <div>{flashCard.question}</div>;
+     const [flip, setFlip] = useState(false);
+     return (
+          <div
+               onClick={() => setFlip(!flip)}
+               className={`card ${flip ? "flip" : " "}`}
+          >
+               <div className="front">
+                    {flashCard.question}
+                    <div className="flashcard-options">
+                         {flashCard.options.map((option, index) => (
+                              <div className="flashcard-option " key={index}>
+                                   {" "}
+                                   {option}
+                              </div>
+                         ))}
+                    </div>
+               </div>
+               <div className="back">nswer {flashCard.answer}</div>
+          </div>
+     );
 }
